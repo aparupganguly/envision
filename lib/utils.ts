2bce9b5ee640201ee08bcd5b5e17a2102375fc6a
aparupganguly <aparupganguly86@gmail.com>
2024-12-17 18:17:51 +0530

    Fixed Depyolment bugs 3

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,5 @@
+/* eslint-disable prefer-const */
+/* eslint-disable no-prototype-builtins */
 import { type ClassValue, clsx } from "clsx";
 import qs from "qs";
 import { twMerge } from "tailwind-merge";
@@ -11,29 +13,51 @@ export function cn(...inputs: ClassValue[]) {
 // ERROR-HANDLER
 export const handleError = (error: unknown) => {
   if (error instanceof Error) {
+    // This is a native JavaScript error (e.g., TypeError, RangeError)
     console.error(error.message);
     throw new Error(`Error: ${error.message}`);
   } else if (typeof error === "string") {
+    // This is a string error message
     console.error(error);
     throw new Error(`Error: ${error}`);
   } else {
+    // This is an unknown type of error
     console.error(error);
     throw new Error(`Unknown error: ${JSON.stringify(error)}`);
   }
 };
 
-// FORMURLQUERY
-export type FormUrlQueryParams = {
-  searchParams: URLSearchParams;
-  key: string;
-  value: string;
-};
+// PLACEHOLDER LOADER - while image is transforming
+const shimmer = (w: number, h: number) => `
+<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+  <defs>
+    <linearGradient id="g">
+      <stop stop-color="#7986AC" offset="20%" />
+      <stop stop-color="#68769e" offset="50%" />
+      <stop stop-color="#7986AC" offset="70%" />
+    </linearGradient>
+  </defs>
+  <rect width="${w}" height="${h}" fill="#7986AC" />
+  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
+  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
+</svg>`;
 
+const toBase64 = (str: string) =>
+  typeof window === "undefined"
+    ? Buffer.from(str).toString("base64")
+    : window.btoa(str);
+
+export const dataUrl = `data:image/svg+xml;base64,${toBase64(
+  shimmer(1000, 1000)
+)}`;
+// ==== End
+
+// FORMURLQUERY
 export const formUrlQuery = ({
   searchParams,
   key,
   value,
-}: FormUrlQueryParams): string => {
+}: FormUrlQueryParams) => {
   const params = { ...qs.parse(searchParams.toString()), [key]: value };
 
   return `${window.location.pathname}?${qs.stringify(params, {
@@ -42,47 +66,38 @@ export const formUrlQuery = ({
 };
 
 // REMOVEKEYFROMQUERY
-export type RemoveUrlQueryParams = {
-  searchParams: URLSearchParams | string;
-  keysToRemove: string[];
-};
-
 export function removeKeysFromQuery({
   searchParams,
   keysToRemove,
-}: RemoveUrlQueryParams): string {
-  const currentUrl = qs.parse(
-    typeof searchParams === "string" ? searchParams : searchParams.toString()
-  );
+}: RemoveUrlQueryParams) {
+  const currentUrl = qs.parse(searchParams);
 
   keysToRemove.forEach((key) => {
     delete currentUrl[key];
   });
 
+  // Remove null or undefined values
+  Object.keys(currentUrl).forEach(
+    (key) => currentUrl[key] == null && delete currentUrl[key]
+  );
+
   return `${window.location.pathname}?${qs.stringify(currentUrl)}`;
 }
 
-// DEBOUNCE
-export const debounce = (func: (...args: unknown[]) => void, delay: number) => {
+// DEBOUNCE (Fixed with Spread Operator)
+export const debounce = (func: (...args: any[]) => void, delay: number) => {
   let timeoutId: ReturnType<typeof setTimeout> | null;
-  return (...args: unknown[]) => {
+  return (...args: any[]) => {
     if (timeoutId) clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => func.apply(null, args), delay);
+    timeoutId = setTimeout(() => func(...args), delay); // Fixed `.apply` to use spread operator
   };
 };
 
 // GETIMAGESIZE
 export type AspectRatioKey = keyof typeof aspectRatioOptions;
-
-export type Image = {
-  aspectRatio?: string;
-  width?: number;
-  height?: number;
-};
-
 export const getImageSize = (
   type: string,
-  image: Image,
+  image: any,
   dimension: "width" | "height"
 ): number => {
   if (type === "fill") {
@@ -94,22 +109,49 @@ export const getImageSize = (
   return image?.[dimension] || 1000;
 };
 
+// DOWNLOADIMAGE
+export const download = (url: string, filename: string) => {
+  if (!url) {
+    throw new Error("Resource URL not provided! You need to provide one");
+  }
+
+  fetch(url)
+    .then((response) => response.blob())
+    .then((blob) => {
+      const blobURL = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = blobURL;
+
+      if (filename && filename.length)
+        a.download = `${filename.replace(" ", "_")}.png`;
+      document.body.appendChild(a);
+      a.click();
+    })
+    .catch((error) => console.log({ error }));
+};
+
 // DEEPMERGEOBJECTS
-export const deepMergeObjects = <T extends Record<string, unknown>>(
-  obj1: T,
-  obj2: T
-): T => {
-  if (!obj2) return obj1;
+export const deepMergeObjects = (obj1: any, obj2: any) => {
+  if (obj2 === null || obj2 === undefined) {
+    return obj1;
+  }
 
-  return Object.keys(obj1).reduce((acc, key) => {
-    const val1 = obj1[key];
-    const val2 = obj2[key];
+  let output = { ...obj2 };
 
-    acc[key] =
-      typeof val1 === "object" && typeof val2 === "object"
-        ? deepMergeObjects(val1 as T, val2 as T)
-        : val1 ?? val2;
+  for (let key in obj1) {
+    if (obj1.hasOwnProperty(key)) {
+      if (
+        obj1[key] &&
+        typeof obj1[key] === "object" &&
+        obj2[key] &&
+        typeof obj2[key] === "object"
+      ) {
+        output[key] = deepMergeObjects(obj1[key], obj2[key]);
+      } else {
+        output[key] = obj1[key];
+      }
+    }
+  }
 
-    return acc;
-  }, { ...obj2 } as T);
+  return output;
 };
