ff792e0adae43561f8343777c2eb45804e0a1c55
aparupganguly <aparupganguly86@gmail.com>
2024-12-17 18:27:07 +0530

    Util updated

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,33 +1,46 @@
-/* eslint-disable prefer-const */
-/* eslint-disable no-prototype-builtins */
-import { type ClassValue, clsx } from "clsx";
-import qs from "qs";
-import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from "clsx"
+import qs from "qs"
+import { twMerge } from "tailwind-merge"
 
-import { aspectRatioOptions } from "@/constants";
+import { aspectRatioOptions } from "@/constants"
+
+// Types
+interface FormUrlQueryParams {
+  searchParams: URLSearchParams
+  key: string
+  value: string
+}
+
+interface RemoveUrlQueryParams {
+  searchParams: URLSearchParams
+  keysToRemove: string[]
+}
+
+interface ImageDimensions {
+  width: number
+  height: number
+  aspectRatio?: keyof typeof aspectRatioOptions
+}
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return twMerge(clsx(inputs))
 }
 
 // ERROR HANDLER
 export const handleError = (error: unknown) => {
   if (error instanceof Error) {
-    // This is a native JavaScript error (e.g., TypeError, RangeError)
-    console.error(error.message);
-    throw new Error(`Error: ${error.message}`);
+    console.error(error.message)
+    throw new Error(`Error: ${error.message}`)
   } else if (typeof error === "string") {
-    // This is a string error message
-    console.error(error);
-    throw new Error(`Error: ${error}`);
+    console.error(error)
+    throw new Error(`Error: ${error}`)
   } else {
-    // This is an unknown type of error
-    console.error(error);
-    throw new Error(`Unknown error: ${JSON.stringify(error)}`);
+    console.error(error)
+    throw new Error(`Unknown error: ${JSON.stringify(error)}`)
   }
-};
+}
 
-// PLACEHOLDER LOADER - while image is transforming
+// PLACEHOLDER LOADER
 const shimmer = (w: number, h: number) => `
 <svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
   <defs>
@@ -40,118 +53,121 @@ const shimmer = (w: number, h: number) => `
   <rect width="${w}" height="${h}" fill="#7986AC" />
   <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
   <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-</svg>`;
+</svg>`
 
 const toBase64 = (str: string) =>
   typeof window === "undefined"
     ? Buffer.from(str).toString("base64")
-    : window.btoa(str);
+    : window.btoa(str)
 
 export const dataUrl = `data:image/svg+xml;base64,${toBase64(
   shimmer(1000, 1000)
-)}`;
-// ==== End
+)}`
 
 // FORM URL QUERY
-export const formUrlQuery = ({
-  searchParams,
-  key,
-  value,
-}: FormUrlQueryParams) => {
-  const params = { ...qs.parse(searchParams.toString()), [key]: value };
+export const formUrlQuery = ({ searchParams, key, value }: FormUrlQueryParams): string => {
+  const params = { ...qs.parse(searchParams.toString()), [key]: value }
 
   return `${window.location.pathname}?${qs.stringify(params, {
     skipNulls: true,
-  })}`;
-};
+  })}`
+}
 
 // REMOVE KEY FROM QUERY
-export function removeKeysFromQuery({
-  searchParams,
-  keysToRemove,
-}: RemoveUrlQueryParams) {
-  const currentUrl = qs.parse(searchParams);
+export function removeKeysFromQuery({ searchParams, keysToRemove }: RemoveUrlQueryParams): string {
+  const currentUrl = qs.parse(searchParams.toString())
 
   keysToRemove.forEach((key) => {
-    delete currentUrl[key];
-  });
+    delete currentUrl[key]
+  })
 
   // Remove null or undefined values
   Object.keys(currentUrl).forEach(
     (key) => currentUrl[key] == null && delete currentUrl[key]
-  );
+  )
 
-  return `${window.location.pathname}?${qs.stringify(currentUrl)}`;
+  return `${window.location.pathname}?${qs.stringify(currentUrl)}`
 }
 
 // DEBOUNCE
-export const debounce = (func: (...args: any[]) => void, delay: number) => {
-  let timeoutId: NodeJS.Timeout | null;
-  return (...args: any[]) => {
-    if (timeoutId) clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => func.apply(null, args), delay);
-  };
-};
+export const debounce = <T extends (...args: Parameters<T>) => ReturnType<T>>(
+  func: T,
+  delay: number
+) => {
+  let timeoutId: NodeJS.Timeout | null
+  
+  return (...args: Parameters<T>) => {
+    if (timeoutId) clearTimeout(timeoutId)
+    timeoutId = setTimeout(() => {
+      func(...args)
+    }, delay)
+  }
+}
 
-// GE IMAGE SIZE
-export type AspectRatioKey = keyof typeof aspectRatioOptions;
+// GET IMAGE SIZE
+export type AspectRatioKey = keyof typeof aspectRatioOptions
 export const getImageSize = (
   type: string,
-  image: any,
+  image: ImageDimensions,
   dimension: "width" | "height"
 ): number => {
   if (type === "fill") {
     return (
-      aspectRatioOptions[image.aspectRatio as AspectRatioKey]?.[dimension] ||
+      aspectRatioOptions[image.aspectRatio as AspectRatioKey]?.[dimension] ??
       1000
-    );
+    )
   }
-  return image?.[dimension] || 1000;
-};
+  return image?.[dimension] ?? 1000
+}
 
 // DOWNLOAD IMAGE
-export const download = (url: string, filename: string) => {
+export const download = (url: string, filename: string): void => {
   if (!url) {
-    throw new Error("Resource URL not provided! You need to provide one");
+    throw new Error("Resource URL not provided! You need to provide one")
   }
 
   fetch(url)
     .then((response) => response.blob())
     .then((blob) => {
-      const blobURL = URL.createObjectURL(blob);
-      const a = document.createElement("a");
-      a.href = blobURL;
-
-      if (filename && filename.length)
-        a.download = `${filename.replace(" ", "_")}.png`;
-      document.body.appendChild(a);
-      a.click();
+      const blobURL = URL.createObjectURL(blob)
+      const a = document.createElement("a")
+      a.href = blobURL
+      
+      if (filename && filename.length) {
+        a.download = `${filename.replace(" ", "_")}.png`
+      }
+      document.body.appendChild(a)
+      a.click()
     })
-    .catch((error) => console.log({ error }));
-};
+    .catch((error) => console.error({ error }))
+}
 
 // DEEP MERGE OBJECTS
-export const deepMergeObjects = (obj1: any, obj2: any) => {
+export const deepMergeObjects = <T extends Record<string, unknown>>(obj1: T, obj2: T | null | undefined): T => {
   if(obj2 === null || obj2 === undefined) {
-    return obj1;
+    return obj1
   }
 
-  let output = { ...obj2 };
+  const output = { ...obj2 }
 
-  for (let key in obj1) {
-    if (obj1.hasOwnProperty(key)) {
+  for (const key in obj1) {
+    if (Object.prototype.hasOwnProperty.call(obj1, key)) {
       if (
         obj1[key] &&
         typeof obj1[key] === "object" &&
         obj2[key] &&
         typeof obj2[key] === "object"
       ) {
-        output[key] = deepMergeObjects(obj1[key], obj2[key]);
+        output[key] = deepMergeObjects(
+          obj1[key] as Record<string, unknown>,
+          obj2[key] as Record<string, unknown>
+        )
       } else {
-        output[key] = obj1[key];
+        output[key] = obj1[key]
       }
     }
   }
 
-  return output;
-};
+  return output as T
+}
+
